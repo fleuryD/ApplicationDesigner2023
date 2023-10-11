a004cd5a8c8118be5ef1d775efe8b39012a24424
fleuryD <fleurydavid31@gmail.com>
2023-10-11 15:40:45 +0200

    Front: router debut

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,51 @@
 // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
 import React from "react"
-import logo from "./logo.svg"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { useAppSelector } from "store/store"
+import NavBar from "ui/NavBar"
+
+// import DevAuthInfos from "features/auth/DevAuthInfos"
+
+// * public
+//import PageLogin from "pages/auth/PageLogin"
+
+// * private
+//import PageHome from "pages/PageHome"
+//import PageUsers from "pages/user/PageUsers"
+//import PageUser from "pages/user/PageUser"
+//import PageWIP from "pages/PageWIP"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+// import "styles/global.scss"
 import "styles/App.css"
 
 // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 export default function App() {
+	const auth = useAppSelector((state) => state.auth)
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div className="App container-xl appStyled " id="container-global">
+			<Router>
+				<NavBar />
+				<>
+					{auth.isConnected ? (
+						<Routes>
+							{/* 					
+							<Route path="/users" element={<PageUsers />} />
+							<Route path="/user/:id" element={<PageUser />} />
+							<Route path="/wip" element={<PageWIP />} />					
+							<Route path="*" element={<PageHome />} />
+							*/}
+						</Routes>
+					) : (
+						<Routes>
+							{/* 
+							<Route path="*" element={<PageLogin />} />
+							*/}
+						</Routes>
+					)}
+				</>
+			</Router>
 		</div>
 	)
 }
