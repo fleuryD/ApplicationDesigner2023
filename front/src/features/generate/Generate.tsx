420435ce94d32925dc63a5834bbe0ff6264c6ffd
zobiwone <zobiwone@gmail.com>
2023-10-16 17:20:52 +0200

    Forms dans modals

---
+++ front/src/features/generate/Generate.tsx
@@ -0,0 +1,32 @@
+// ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+import React from "react"
+//import { useAppDispatch } from "store/store"
+import { Entite } from "types"
+import UmlAttribut from "features/uml/UmlAttribut"
+import ButtonEditEntite from "features/entites/ButtonEditEntite"
+import ButtonCreateAttribut from "features/attributs/ButtonCreateAttribut"
+//import FormAttribut from "features/attributs/FormAttribut"
+
+// ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+type Props = {
+	entite: Entite
+}
+
+export default function Generate({ entite }: Props) {
+	return (
+		<div className="umlEntity col-12 col-md-6 col-lg-4">
+			<div className="umlEntityInner">
+				<h2>
+					{entite.name} <ButtonEditEntite entite={entite} className="btn-sm float-end" />
+				</h2>
+				{entite.attributs.map((attr: any) => (
+					<UmlAttribut key={"attr" + attr.id} attribut={attr} />
+				))}
+
+				<ButtonCreateAttribut className="btn-sm" entite={entite} />
+			</div>
+		</div>
+	)
+}
