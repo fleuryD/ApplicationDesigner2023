bec9fc41cb0fbfaa5b8aa89099b240484c79bff6
fleuryD <fleurydavid31@gmail.com>
2023-10-30 11:12:57 +0100

    update: templateCppHpp

--- front/src/features/generate/templateCppHpp.ts
+++ front/src/features/generate/templateCppHpp.ts
@@ -12,32 +12,36 @@ type Props = {
 }
 
 export default function templateCppHpp({ entite, entitePascalName, entiteCamelName, entiteCamelNamePluriel }: Props) {
-	let code = `\n`
-	code += `#pragma once  \n`
-	code += `\n`
-	code += `class ${entitePascalName}  \n`
-	code += `{  \n`
-	code += `    private :  \n`
+	let code = `
+#pragma once
+
+class ${entitePascalName}
+{
+    private :  \n`
+
 	entite.attributs.map((attr: any) => {
 		code += `        ${attr.tipe}: _${attr.name};  \n`
 		return code
 	})
-	code += ` \n`
-	code += `    public :  \n`
-	code += `        ${entitePascalName}();  \n`
-	code += `        ${entitePascalName}(const ${entitePascalName} &other);  \n`
-	code += `        ~${entitePascalName}(void);  \n`
-	code += `        ${entitePascalName} &operator=(const ${entitePascalName} &other);  \n`
 
-	code += ` \n`
-	code += `    // get/set :  \n`
+	code += `
+    public :
+        ${entitePascalName}();
+        ${entitePascalName}(const ${entitePascalName} &other);
+        ~${entitePascalName}(void);
+        ${entitePascalName} &operator=(const ${entitePascalName} &other);
+
+        // get/set :  \n`
+
 	entite.attributs.map((attr: any) => {
 		code += `        ${attr.tipe}	get${toPascalCase(attr.name)}(void) const;  \n`
 		code += `        void	set${toPascalCase(attr.name)}(${attr.tipe} val) const;  \n\n`
 		return code
 	})
-	code += `};  \n`
-	code += `std::ostream &operator<<(std::ostream &flux, const ${entitePascalName} &${entiteCamelName});  \n`
+
+
+	code += `};
+std::ostream &operator<<(std::ostream &flux, const ${entitePascalName} &${entiteCamelName});  \n`
 
 	return {
 		code,
